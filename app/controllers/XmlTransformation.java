aa76c2d081ea3964fe1cf9c489691905d32ec9b3
Sylwester Gryzio <sylwester.gryzio@gmail.com>
2014-11-29 13:51:26 +0100

    Added XML to Json transformation - no xml modifications

--- app/controllers/XmlTransformation.java
+++ app/controllers/XmlTransformation.java
@@ -1,9 +1,14 @@
 package controllers;
 
-import play.*;
+import com.google.common.base.Joiner;
 import play.mvc.*;
-
 import views.html.*;
+import org.json.*;
+
+import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.List;
 
 public class XmlTransformation extends Controller {
 
@@ -12,6 +17,20 @@ public class XmlTransformation extends Controller {
     }
 
     public static Result process() {
+        try {
+
+            File file = request().body().asMultipartFormData().getFile("pkw-data").getFile();
+            List<String> lines = Files.readAllLines(file.toPath(), Charset.forName("UTF-8"));
+
+            String xmlContent = Joiner.on("").join(lines);
+
+            JSONObject asJson = XML.toJSONObject(xmlContent);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return internalServerError(e.getMessage());
+        }
+
         return ok(xml.render());
     }
 }
