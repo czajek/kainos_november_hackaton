30feed73d652e56e6906e186fd86bf357bd4f6a1
kpakur <k.pakur@kainos.com>
2014-11-29 14:20:59 +0100

    some mock data

--- app/controllers/Results.java
+++ app/controllers/Results.java
@@ -3,6 +3,10 @@ package controllers;
 import play.*;
 import play.mvc.*;
 import views.html.*;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
 
 public class Results extends Controller {
 
@@ -10,8 +14,49 @@ public class Results extends Controller {
         return ok(results.render());
     }
 
-    public static Result circumference(int number){
-        return ok("some data");
+
+    public static Result circumference(int number) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode obj = mapper.readTree("{\n" +
+                " \"nazwa\": \"Gdańsk\",\n" +
+                " \"wojewodztwo\": \"Pomorskie\",\n" +
+                " \"okrag\": [\n" +
+                "  {\n" +
+                "   \"numer\": \"1\",\n" +
+                "   \"obwod\": [\n" +
+                "    {\n" +
+                "     \"nazwa\": \"Brzeźno\",\n" +
+                "     \"karta\": [\n" +
+                "      {\n" +
+                "       \"rodzaj\": \"Rada Miasta\",\n" +
+                "       \"lista\": [\n" +
+                "        {\n" +
+                "         \"numer\": \"8\",\n" +
+                "         \"nazwa\": \"KW Liga Polskich Rodzin\",\n" +
+                "         \"kandydat\": [\n" +
+                "          {\n" +
+                "           \"imie\": \"Marzena\",\n" +
+                "           \"nazwisko\": \"Grabowska\",\n" +
+                "           \"id\": 1\n" +
+                "          },\n" +
+                "          {\n" +
+                "           \"imie\": \"Jarosław\",\n" +
+                "           \"nazwisko\": \"Dariusz-Piotr\",\n" +
+                "           \"id\": 2,\n" +
+                "           \"votes\": 2134\n" +
+                "          }\n" +
+                "         ]\n" +
+                "        }\n" +
+                "       ]\n" +
+                "      }\n" +
+                "     ]\n" +
+                "    }\n" +
+                "   ]\n" +
+                "  }\n" +
+                " ]\n" +
+                "}");
+
+        return ok(obj);
     }
 
 }
