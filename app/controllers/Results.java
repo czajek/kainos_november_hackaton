a8f6442a7c9d4205e3963ab636818169aeabf0fc
kpakur <k.pakur@kainos.com>
2014-11-29 13:58:24 +0100

    simple view

--- app/controllers/Results.java
+++ app/controllers/Results.java
@@ -2,12 +2,16 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-import views.html.results.*;
+import views.html.*;
 
 public class Results extends Controller {
 
     public static Result index() {
-        return ok(index.render());
+        return ok(results.render());
+    }
+
+    public static Result circumference(int number){
+        return ok("some data");
     }
 
 }
